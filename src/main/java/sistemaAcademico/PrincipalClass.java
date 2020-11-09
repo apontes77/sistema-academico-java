02c981351ab9f7ae9aad8247afd41885c1d854cd
apontes77 <alexandrepontesgyn@gmail.com>
2020-11-09 20:33:37 -0300

    fix: deleting UserService class for refactor process

--- src/main/java/sistemaAcademico/PrincipalClass.java
+++ src/main/java/sistemaAcademico/PrincipalClass.java
@@ -5,13 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PrincipalClass{
-
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(PrincipalClass.class, args);
 	}
-
-
-
 }
